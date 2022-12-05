6b667ef861f5252d4ab32174ace0d77deed3c7ca
Dan-Stefan Dragan <dan.dragan1992@gmail.com>
2022-12-05 13:28:09 +0200

    Add solution for day5

---
+++ day5/day5_pt1.rs
@@ -0,0 +1,76 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::str::FromStr;
+
+fn main() {
+    if let Ok(lines) = read_lines("day5.txt".to_string()) {
+
+        let mut cols = std::iter::repeat(vec![]).take(9).collect::<Vec<_>>();
+        let mut r_cols = std::iter::repeat(vec![]).take(9).collect::<Vec<_>>();
+        let mut nums = Vec::new();
+        let mut froms = Vec::new();
+        let mut tos = Vec::new();
+
+        for line in lines {
+            let s_line = line.unwrap();
+
+            if s_line.contains('[') {
+                for (i, c) in s_line.chars().enumerate() {
+                    if c.is_alphabetic() {
+                        cols[(i-1)/4].push(c);
+                    }
+                }
+            }
+            else if s_line.contains("move") {
+                let v_line: Vec<&str> = s_line.split(" ").collect();
+
+                let mut x = 0;
+
+                for elem in v_line {
+                    if parsable::<usize>(elem) {
+                        if x == 0 {
+                            nums.push(elem.parse::<usize>().unwrap());
+                        }
+                        else if x == 1 {
+                            froms.push(elem.parse::<usize>().unwrap());
+                        }
+                        else if x == 2 {
+                            tos.push(elem.parse::<usize>().unwrap());
+                        }
+
+                        x += 1;
+                    }
+                }
+            }
+        }
+
+        for i in 0..cols.len() {
+            while !cols[i].is_empty() {
+                let a = cols[i].pop().unwrap();
+                r_cols[i].push(a);
+            }
+        }
+
+        for i in 0..nums.len() {
+            for _j in 0..nums[i] { 
+                let a = r_cols[froms[i]-1].pop().unwrap();
+                r_cols[tos[i]-1].push(a);
+            }
+        }
+
+        for c in r_cols {
+            print!("{}", c[c.len()-1]);
+        }
+        println!();
+    }
+}
+
+fn parsable<T: FromStr>(s: &str) -> bool {
+    s.parse::<T>().is_ok()
+}
+
+fn read_lines(filename : String) -> io::Result<io::Lines<io::BufReader<File>>>
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
